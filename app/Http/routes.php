8a454cedcc97f0eed3a084090da6df5178e33265
user <Global@mail.ua>
2018-05-22 20:38:27 +0300

    forms

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,44 @@
 <?php
 
-  use App\Task;
-  use Illuminate\Http\Request;
+use App\Task;
+use Illuminate\Http\Request;
 
-  /**
-   * Вывести панель с задачами
-   */
-  Route::get('/', function () {
-      return view('tasks');
-  });
+/**
+ * 
+ */
+Route::get('/', function () {
+    //
+    $tasks= Task::all();
+    return view('tasks',[
+	'tasks'=>$tasks
+    ]);
+});
 
-  /**
-   * Добавить новую задачу
-   */
-  Route::post('/task', function (Request $request) {
+/**
+ * Добавить новую задачу
+ */
+Route::post('/task', function (Request $request) {
+    //проверка данных
+    $validator = Validator::make($request->all(), [
+		'name' => 'required|min:5|max:255',
+    ]);
+
+    if ($validator->fails()) {
+	return redirect('/')
+			->withInput()
+			->withErrors($validator);
+    }
       $task=new Task();
       $task->name=$request->name;
       $task->save();
-      exit('save');
-  });
+      return redirect('/');
+});
 
-  /**
-   * Удалить задачу
-   */
-  Route::delete('/task/{task}', function (Task $task) {
+/**
+ * Удалить задачу
+ */
+Route::delete('/task/{task}', function (Task $task) {
     //
-  });
+    $task->delete();
+    return redirect('/');
+});
